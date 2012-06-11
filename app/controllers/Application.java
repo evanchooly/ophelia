c4f03a27d194814461c31154f69c505b09dd03de
evanchooly <jlee@antwerkz.com>
2012-06-10 22:33:29 -0400

    starting layout

---
+++ app/controllers/Application.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.modules.router.Post;
+import play.mvc.Controller;
+
+public class Application extends Controller {
+  public static void index() {
+    render();
+  }
+
+  @Post("/query")
+  public static void query(String query) {
+    render();
+  }
+}
