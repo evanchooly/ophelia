c969daa4f400480ebb468d7059213e67199818c9
evanchooly <jlee@antwerkz.com>
2012-09-06 23:56:32 -0400

    clean up

--- project/Build.scala
+++ project/Build.scala
@@ -4,13 +4,10 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
   val appName = "ophelia"
-  val appVersion = "1.0-SNAPSHOT"
+  val appVersion = "0.4-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
     "com.antwerkz.sofia" % "sofia-play" % "0.11",
-//    "com.fasterxml.jackson.core" % "jackson-databind" % "2.0.5",
-//    "com.google.code.gson" % "gson" % "1.7.1",
     "org.mongodb" % "mongo-java-driver" % "2.7.3"
   )
 
