dd3b8fc4c2138172218c443f23c9e84b08cfed5e
evanchooly <jlee@antwerkz.com>
2012-11-03 22:35:23 -0400

    clean up

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -51,12 +51,3 @@ function initOphelia() {
         });
     });
 }
-function getMethods(obj) {
-    var res = [];
-    for (var m in obj) {
-        if (typeof obj[m] == "function") {
-            res.push(obj[m])
-        }
-    }
-    return res;
-}
