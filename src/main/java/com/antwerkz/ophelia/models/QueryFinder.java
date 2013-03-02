e6df96829150ac9aae2f35065ec4a68b61e7dbb4
evanchooly <jlee@antwerkz.com>
2013-03-01 21:48:17 -0500

    adding some project/copyright info

--- src/main/java/com/antwerkz/ophelia/models/QueryFinder.java
+++ src/main/java/com/antwerkz/ophelia/models/QueryFinder.java
@@ -1,4 +1,21 @@
+/**
+ * Copyright (C) 2012-2013 Justin Lee <jlee@antwerkz.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.antwerkz.ophelia.models;
+
+import java.util.List;
 
 import com.antwerkz.ophelia.dao.Finder;
 import com.antwerkz.ophelia.dao.MongoModel;
@@ -6,8 +23,6 @@ import com.antwerkz.ophelia.dao.MongoModel.Operation;
 import com.antwerkz.ophelia.models.criteria.QueryCriteria;
 import com.google.code.morphia.Datastore;
 import org.bson.types.ObjectId;
-
-import java.util.List;
 
 public class QueryFinder extends Finder<Query> {
     public QueryFinder() {
@@ -20,8 +35,8 @@ public class QueryFinder extends Finder<Query> {
             public Query execute(Datastore ds) {
                 QueryCriteria criteria = new QueryCriteria(ds);
                 criteria.and(
-                        criteria.bookmark().equal(bookmark),
-                        criteria.database().equal(database)
+                    criteria.bookmark().equal(bookmark),
+                    criteria.database().equal(database)
                 );
                 return criteria.query().get();
             }
